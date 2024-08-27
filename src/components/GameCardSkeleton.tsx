87305cd0a146e3d6cbde58f4985186701934a4fa
LMIsaksen <lisemari.isaksen@gmail.com>
2024-08-27 10:32:47 +0200

    removed duplicated styles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="300px" borderRadius={10} overflow={"hidden"}>
-      <Skeleton height="240px" />
+    <Card>
+      <Skeleton height="200px" />
       <CardBody>
         <SkeletonText />
       </CardBody>
